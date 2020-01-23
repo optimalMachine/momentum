577b7d68bfee047b1290f80f5df69a45428cd86c
Jiuk Choi <jiukchoi@cjuartintelligence.com>
2020-01-23 08:51:19 -0600

    fourth commit
    another few updates...

--- clock.js
+++ clock.js
@@ -1,8 +1,19 @@
 const clockContainer = document.querySelector('.js-clock');
-const clockTitle = 
+const clockTitle = clockContainer.querySelector('h1');
+
+
+function getTime() {
+    const date = new Date();
+
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+
+}
 
 function init(){
-
+    getTime();
 }
 
 init();
